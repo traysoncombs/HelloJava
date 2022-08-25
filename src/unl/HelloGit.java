65a4ab43eda6a2d115703c1871666e15b60cd83b
adrie <adrie@10.65.13.91>
2022-08-25 14:03:04 -0500

    Partner Commit

--- src/unl/HelloGit.java
+++ src/unl/HelloGit.java
@@ -3,5 +3,7 @@ package unl;
 public class HelloGit {
     public static void Main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Adrien Palomo\n");
+        System.out.println("Computer Science major \n");
     }
 }
